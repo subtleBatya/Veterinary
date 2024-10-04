bd8ffb3b30bbf6dc9bf093f241a95e342338e919
subtleBatya <batyrsazakow.2003@gmail.com>
2024-10-04 19:05:42 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {API_URL} from '../api/BrandApi'
 
-import React from 'react'
-// import Navbar from "./components/Navbar";
 
+const Home = () => {
+  
 
-// import RecentBlogs from "./pages/RecentBlogs";
-// import Footer from "./Footer";
-// import Navbar from '../components/navbar/Navbar';
-import MainSlider from '../components/test/MainSlider';
-// import Header from '../components/Header/Header'
-import News from '../components/test/News';
-import Categories from '../components/test/Categories';
-import Newsletters from '../components/test/Newsletters';
-import Multimedia from '../components/test/Multimedia';
-import Gallery from '../components/test/Gallery';
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      
+    </div>
+  );
+};
 
-function Home() {
-    return (
-        <div className=''>
-            
-            <MainSlider/>
-            <News/>
-            <Categories/>
-            <Newsletters/>
-            <Multimedia/>
-            <Gallery/>  
-            {/* <About /> */}
-            {/* <Courses /> */}
-            {/* <RecentBlogs/> */}
-        </div>
-    )
-}
-
-export default Home
-
+export default Home;
 
 
 

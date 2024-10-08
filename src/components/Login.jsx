9bddabca3a71af2b853ea4edd9699e37db4ab484
ylham0505 <serdar.garyagdyyew204@gmail.com>
2024-10-08 17:06:16 +0500

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,24 +25,94 @@ const Login = () => {
   };
   
   return (
-    <div className=" flex flex-col  mx-auto w-1/3 gap-3">
-      <h1>{isAdmin ? 'Welcome Admin' : 'Please Log in'}</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    // <div className=" flex flex-col  mx-auto w-1/3 gap-3">
+    //   <h1>{isAdmin ? 'Welcome Admin' : 'Please Log in'}</h1>
+    //   <input
+    //     type="text"
+    //     placeholder="Username"
+    //     value={username}
+    //     onChange={(e) => setUsername(e.target.value)}
+    //   />
+    //   <input
+    //     type="password"
+    //     placeholder="Password"
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //   />
+    //   <button onClick={handleLogin}>Login</button>
       
-      {error && <p>{error}</p>}
-    </div>
+    //   {error && <p>{error}</p>}
+    // </div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img
+            alt="Your Company"
+            src="Logo.png"
+            className="ms-[170px] h-10 w-auto"
+          /> */}
+          <img
+            alt="Your Company"
+            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          {isAdmin ? 'Welcome Admin' : 'Sign in to your account'}
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  autoComplete="username"
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                onClick={handleLogin}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+        {error && <p>{error}</p>}
+      </div>
   );
 };
 

bc0b7bf5502fa240f718e58deea2eb323020ce5c
ylham0505 <serdar.garyagdyyew204@gmail.com>
2024-10-24 16:37:18 +0500

    update

--- src/pages/ProductShow.jsx
+++ src/pages/ProductShow.jsx
@@ -2,13 +2,57 @@ import React from "react";
 
 function ProductShow() {
   return(
-    <div className="max-w-7xl">
-        <div className="grid grid-cols-2">
+    <div className="max-w-6xl m-auto gap-3">
+        <div className="grid grid-cols-2 my-4">
+            <div className="text-center">
+                <img className="w-full h-96" src="photo.png" alt="image" />
+            </div>
+            <div className="ml-10">
+                <h1 className="text-xl font-bold pb-3">Product name</h1>
+                <span className="font-medium">price TMT</span>
+                <p className="py-3">description Lorem ipsum dolor sit amet consectetur adipisicing elit. At optio maiores sit unde veritatis doloribus quas debitis voluptate, quos iusto ipsum voluptatem consectetur quidem officiis perspiciatis cumque nihil ad qui?</p>
+                <div className="flex items-center gap-3">
+                    <img className="w-28 rounded-full" src="Logo.png" alt="" />
+                    <span>brand name</span>
+                </div>
+                <p className="py-2">Category name</p>
+                <hr className="border-2 my-3"/>
+                <div className="flex">
+                    <div className="border rounded-md">
+                        <button className="border-r-2 px-3 py-2 font-medium w-[40px] h-[44px]">-</button>
+                        <span className="px-4">0</span>
+                        <button className="rounded-r-md w-[41px] h-[44px] bg-[#DB4444] text-white">+</button>
+                    </div>
+                    <div className="mx-4">
+                        <button className="border rounded-md w-[165px] h-[44px] bg-[#DB4444] text-white">Add to cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="grid grid-cols-4 gap-3">
             <div>
-                vkdmvkdmkv
+                <img className="w-full rounded-md border-2" src="photo.png" alt="" />
+                <p className="mb-3 mt-2 font-semibold h-3">Product name</p>
+                <p className="mb-3">pricee</p>
+                <button className="w-full text-white border rounded-md bg-black py-2">Add to cart</button>
             </div>
             <div>
-                kjejnkcej
+                <img className="w-full rounded-md border-2" src="photo.png" alt="" />
+                <p className="mb-3 mt-2 font-semibold h-3">Product name</p>
+                <p className="mb-3">pricee</p>
+                <button className="w-full text-white border rounded-md bg-black py-2">Add to cart</button>
+            </div>
+            <div>
+                <img className="w-full rounded-md border-2" src="photo.png" alt="" />
+                <p className="mb-3 mt-2 font-semibold h-3">Product name</p>
+                <p className="mb-3">pricee</p>
+                <button className="w-full text-white border rounded-md bg-black py-2">Add to cart</button>
+            </div>
+            <div>
+                <img className="w-full rounded-md border-2" src="photo.png" alt="" />
+                <p className="mb-3 mt-2 font-semibold h-3">Product name</p>
+                <p className="mb-3">pricee</p>
+                <button className="w-full text-white border rounded-md bg-black py-2">Add to cart</button>
             </div>
         </div>
     </div>
